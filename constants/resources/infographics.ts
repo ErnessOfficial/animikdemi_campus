5da2ed91c3ee809269f278f9e72d560fc1c94360
ErnessOfficial <entrepalabrasurgentes@gmail.com>
2025-09-30 01:37:52 +0100

    actualizacion de  enlace  en infografias

--- constants/resources/infographics.ts
+++ constants/resources/infographics.ts
@@ -8,7 +8,7 @@ export const infographics: ResourceItem[] = [
   },
   {
     title: 'Infografía: Respiración Consciente (Guía Visual)',
-    url: 'https://example.com/infografia-respiracion-consciente'
+    url: 'https://drive.google.com/file/d/19bTYjz3G-I_yPmK5q6r1-t9bOGbiSrDI/view?usp=sharing'
   },
   {
     title: 'Infografía: Comunicación Asertiva — Pasos Prácticos',
